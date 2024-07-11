ed03a2aba60476d805a6c232f99f8e179f817c55
Mitchell Tillman <mtillman14@gmail.com>
2024-07-11 11:48:07 -0400

    Initial Commit

---
+++ Timeliner/TimelinerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelinerApp.swift
+//  Timeliner
+//
+//  Created by Mitchell Tillman on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TimelinerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
